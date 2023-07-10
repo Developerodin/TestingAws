001741b987637cd4cb9db6d678377c7091996d96
Akshaypareek01 <akshay96102@gmail.com>
2023-07-10 16:25:33 +0530

    working

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const io = new Server(server,{cors :{origin: "http://localhost:3000"}});
 // io.listen(8000);
 app.get('/',(req,res)=>{
   res.send({
-    message:"Backen is Working"
+    message:"Backen is Working with aws"
   })
 })
 app.get('/api/token', (req, res) => {
